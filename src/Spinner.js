4df2bf9fffabf5b58b48bb76a9de16e7ed01b646
fanik05 <fatinanik129@gmail.com>
2021-10-11 21:58:15 +0600

    states, lifecycle methods and conditional rendering

---
+++ src/Spinner.js
@@ -0,0 +1,13 @@
+const Spinner = props => {
+    return (
+        <div class="ui active dimmer">
+            <div class="ui big text loader">{props.message}</div>
+        </div>
+    )
+}
+
+Spinner.defaultProps = {
+    message: 'Loading...'
+}
+
+export default Spinner
